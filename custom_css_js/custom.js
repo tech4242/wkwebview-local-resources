6a1fa5fc3cecaafd632d2a1eb689b28b54a84952
Nikolay Dimolarov <dimolarov@hotmail.com>
2018-04-19 09:10:46 +0200

    changed custom files

---
+++ custom_css_js/custom.js
@@ -0,0 +1,7 @@
+function test() {
+    if(document.getElementById("webkit-h1").style.color == "lightblue") {
+        document.getElementById("webkit-h1").style.color = "coral";
+    } else {
+        document.getElementById("webkit-h1").style.color = "lightblue";
+    }
+}
